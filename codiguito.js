3de5ba31cab80a64c9203a6d82a49eaf1c2e5e74
Leonidas Esteban <leonidasesteban@gmail.com>
2012-03-21 19:56:25 -0500

    app mejorandola

---
+++ codiguito.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function() {
+
+	var alto = window.innerHeight;
+	/*$('.cuerpo').css('min-height',alto)*/
+	$('#messages').css('height',alto-alto*0.40)
+
+	/*alert(alto);*/
+});
